159a14212184dc9a1e9028d99ab7fa69181a0c65
Koirala88 <61165300+Koirala88@users.noreply.github.com>
2020-02-17 14:50:41 -0600

    Solution for Assignments

---
+++ Module5_sol1.py
@@ -0,0 +1,19 @@
+#Question 1
+
+print_code_steps = """
+import math
+def add (x,y):
+	sum =x+y
+	print("The sum of " + str(x) + " plus " + str(y) + " = " + str(sum))
+add(10,40)
+""" 
+print(print_code_steps)
+
+exec(print_code_steps)
+
+
+
+import math
+def add (x,y):
+	sum = x+y
+	print("The sum of " + str(x) + " plus " + str(y) + " = " + str(sum))
